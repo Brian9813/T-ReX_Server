14708fa6050cc242875df04974065b8e9036ce71
Brian9813 <brian.m.rothschild@gmail.com>
2020-05-02 12:07:07 -0500

    Added all deletes by primary keys

---
+++ tableLayout/delete/byTableNumber.php
@@ -0,0 +1,44 @@
+<?php
+
+			/*This webservice will delete a  user in the database
+			 *
+			 *
+			 * */
+
+            if( ! array_key_exists('tableNumber', $_GET))
+            {
+                exit('Missing tableNumber');
+			}
+
+			//credentials for connection
+			$dbhost = 'localhost';
+			$db = 'capstone';
+			$user = 'user1';
+			$pswd = 'password';
+			//echo 'Before Connect<br>';
+	
+			//open connection to mysql
+			$con = mysqli_connect($dbhost, $user, $pswd, $db);
+
+
+			if(! $con )
+			{
+				die('Could not connect -> '.$con->connect_error);
+			}
+
+
+			//Query with the column and the value passed via the parameters
+			$sql  = 'DELETE FROM `TableLayout` WHERE `tableNumber` = ' . $_GET['tableNumber'];
+
+
+			//echo print_r($sql);
+			
+			//result of the query, also runs the query
+			$res = mysqli_query($con, $sql);
+			
+            echo json_encode($res);
+
+
+			//close connection
+			mysqli_close($con);
+		?>
