949054a42a3561c3f873864c263068c27a4d0984
Brian9813 <brian.m.rothschild@gmail.com>
2020-05-03 14:48:12 -0500

    Added Coaster WS

---
+++ custom/fullCoaster.php
@@ -0,0 +1,89 @@
+<?php
+
+			/*This web service is for one column and one value
+			 *
+			 *
+			 * */
+
+
+
+			//print all of the parameters
+			//echo print_r($_GET[value]);
+			//echo print_r($_GET[column]);
+
+			//make sure the value and column parms are there 
+			//if not the program will exit with an error message 
+
+			if( ! array_key_exists('coasterId', $_GET))
+			{	
+				exit('Missing value parameter');
+			}
+			
+
+
+
+
+
+			//credentials for connection
+			$dbhost = 'localhost';
+			$db = 'capstone';
+			$user = 'user1';
+			$pswd = 'password';
+			//echo 'Before Connect<br>';
+	
+			//open connection to mysql
+			$con = mysqli_connect($dbhost, $user, $pswd, $db);
+
+
+			if(! $con )
+			{
+				die('Could not connect -> '.$con->connect_error);
+			}
+
+
+			//Query with the column and the value passed via the parameters
+			$sql  = 'SELECT * from Coaster C JOIN DrinkInfo D ON C.drinkId = D.drinkId WHERE C.coasterId = ' . $_GET['coasterId'];
+
+			//echo print_r($sql);
+			
+			//result of the query, also runs the query
+			$res = mysqli_query($con, $sql);
+			
+			
+
+			//Load results into array
+			if( mysqli_num_rows($res) > 0 )
+			{
+				//declare an array to hold the resulting rows from the query 
+				
+				$rows = array();
+
+
+
+				//loop through rows of the result of the query
+				while ( $r = mysqli_fetch_assoc($res) )
+				{
+
+					array_push($rows, $r);
+				}
+
+				//return the array of the result rows
+				//
+				echo json_encode($rows);
+			}
+			else
+			{
+				//return an empty json object if there are no results and exit the program
+				echo '{}';
+			}
+
+			
+
+			
+			
+
+
+
+			//close connection
+			mysqli_close($con);
+		?>
